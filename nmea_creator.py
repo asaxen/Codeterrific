128673cfe5b1e21731832ee95ce6a8468fd40c4d
Adam Saxén <adam.saxen@gmail.com>
2020-02-12 19:42:25 +0100

    Update nmea_creator.py

--- nmea_creator.py
+++ nmea_creator.py
@@ -1,3 +1,4 @@
+
 def computeChecksum(nmea):
   checksum = 0
   for s in nmea:
@@ -28,5 +29,12 @@ for i, d in enumerate(data):
   minutesE = (d[1] % 1)*60
   degreesE = str(int(d[1]))
   t = 162445.11+i
-  nmea = "GNGGA,"+str(t)+","+degreesN+"{0:.5f}".format(minutesN)+",N,"+degreesE+"{0:.5f}".format(minutesE)+",E,1,12,1.83,106.5,M,34.1,M,,"
+  '''
+  Where a numeric latitude or longitude is given, the two digits immediately to the left of the decimal point are whole minutes, to the right are decimals of minutes, and the remaining digits to the left of the whole minutes are whole degrees.
+
+  Eg. 4533.35 is 45 degrees and 33.35 minutes. ".35" of a minute is exactly 21 seconds.
+
+  Eg. 16708.033 is 167 degrees and 8.033 minutes. ".033" of a minute is about 2 seconds.
+  '''
+  nmea = "GNGGA,"+str(t)+","+degreesN+"{0:.5f}".format(minutesN)+",N,"+degreesE+"0{0:.5f}".format(minutesE)+",E,1,12,1.83,106.5,M,34.1,M,,"
   print("$"+nmea+"*"+computeChecksum(nmea))
